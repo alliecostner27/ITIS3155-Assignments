41e70a7e0a19e953c044e92d82248232cbd3aa10
alliecostner27 <alliecostner3@gmail.com>
2024-09-25 14:41:56 -0400

    Delete BankAccount

--- bankAccount/savingsAccount.py
+++
@@ -1,8 +0,0 @@
-from main import bankAccount
-
-class savingsAccount(bankAccount):
-    
-    def interestRate(self, interestAmount):
-        interestAmount = float(input("Enter the amount you want to deposit: "))
-        self.balance += interestAmount
-        return "Interest deposited. Your new balance is: " + str(self.balance)
